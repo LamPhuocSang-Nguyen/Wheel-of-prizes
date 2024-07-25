b931d1cdb7d6f812c9829e839eebdab250807460
LamPhuocSang-Nguyen <75714558+LamPhuocSang-Nguyen@users.noreply.github.com>
2024-07-25 23:58:54 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import "./styles.css";
+import WheelComponent from "react-wheel-of-prizes";
+import "react-wheel-of-prizes/dist/index.css";
+export default function App() {
+  const segments = [
+    "better luck next time",
+    "won 70",
+    "won 10",
+    "better luck next time",
+    "won 2",
+    "won uber pass",
+    "better luck next time",
+    "won a voucher"
+  ];
+  const segColors = [
+    "#EE4040",
+    "#F0CF50",
+    "#815CD1",
+    "#3DA5E0",
+    "#34A24F",
+    "#F9AA1F",
+    "#EC3F3F",
+    "#FF9000"
+  ];
+  const onFinished = (winner) => {
+    console.log(winner);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <WheelComponent
+      segments={segments}
+      segColors={segColors}
+      winningSegment="better luck next time"
+      onFinished={(winner) => onFinished(winner)}
+      primaryColor="black"
+      contrastColor="white"
+      buttonText="Spin"
+      isOnlyOnce={true}
+    />
   );
 }
-
-export default App;
